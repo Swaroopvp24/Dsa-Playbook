6882933c1237df4ce972e1c19a61d0175b54f379
Swaroopvp24 <157394466+Swaroopvp24@users.noreply.github.com>
2026-07-11 14:40:53 +0530

    Update solution for valid-parentheses (attempt_1.java)

--- Leetcode/0020_valid-parentheses/attempt_1.java
+++ Leetcode/0020_valid-parentheses/attempt_1.java
@@ -1,4 +1,4 @@
-//Trying Attempt 1 filename again without reload 
+//Trying Attempt 1 filename again without reload on the other section of page(submissions)
 class Solution {
     public boolean isValid(String s) {
         // Top index of our custom stack (-1 means empty)
