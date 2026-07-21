cc0b1261104c5f277bab69af1621a2f6024a2a6c
Swaroopvp24 <157394466+Swaroopvp24@users.noreply.github.com>
2026-07-21 23:11:02 +0530

    Add solution for combination-sum (attempt_1_modified.java)

---
+++ Leetcode/0039_combination-sum/attempt_1_modified.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<List<Integer>> res;
+
+    public void comb(int idx, int target, List<Integer> arr, int[] c) {
+        if (target == 0) {
+            res.add(new ArrayList<>(arr));
+            return;
+        } else if (target < 0 || idx >= c.length)
+            return;
+        if (c[idx] <= target) {
+            arr.add(c[idx]);
+            comb(idx, target - c[idx], arr, c);
+            arr.remove(arr.size() - 1);
+        }
+        comb(idx + 1, target, arr, c);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        res = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+        comb(0, target, arr, candidates);
+        return res;
+    }
+}
