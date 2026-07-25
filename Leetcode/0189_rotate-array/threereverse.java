fd1abfb42df51ab09471206b7d33e3a0aa623899
Swaroopvp24 <157394466+Swaroopvp24@users.noreply.github.com>
2026-07-26 00:27:02 +0530

    Add solution for rotate-array (threereverse.java)

---
+++ Leetcode/0189_rotate-array/threereverse.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rev(int[] arr, int st, int en) {
+        while (st < en) {
+            arr[st] = arr[st] ^ arr[en];
+            arr[en] = arr[st] ^ arr[en];
+            arr[st] = arr[st] ^ arr[en];
+
+            st++;
+            en--;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        k = k%nums.length; //VERY VERY IMPORTANT
+        rev(nums,0,nums.length-1); // rverses the whole array
+        rev(nums,0,k-1); // reverses the first k ele
+        rev(nums,k,nums.length-1); //reverse the remaining
+    }
+}
+
+/* We need to swap two contiguous blocks (A and B).
+Reversing the entire array automatically swaps their positions.
+The only problem is that both blocks become reversed.
+Reversing each block again restores their original order.
+*/
