28912f7640781f9632fdf76b80e9d842b3bf59cd
Swaroopvp24 <157394466+Swaroopvp24@users.noreply.github.com>
2026-08-14 19:56:39 +0530

    Add solution for first-missing-positive (attempt_1.java)

---
+++ Neetcode/first-missing-positive/attempt_1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= 0 || nums[i] > nums.length) {
+                continue;
+            }
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != (i + 1))
+                return i + 1;
+        }
+        return nums.length + 1;
+    }
+}
