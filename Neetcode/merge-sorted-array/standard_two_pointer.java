11b4cd55ac683671369d1ab5806c9a8b65f5b0b4
Swaroopvp24 <157394466+Swaroopvp24@users.noreply.github.com>
2026-08-16 21:59:39 +0530

    Add solution for merge-sorted-array (standard_two_pointer.java)

---
+++ Neetcode/merge-sorted-array/standard_two_pointer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int nums1Index = m - 1;
+        int nums2Index = n - 1;
+        int mergedIndex = m + n - 1;
+
+        while (nums2Index >= 0) {
+            if (nums1Index >= 0 && nums1[nums1Index] > nums2[nums2Index]) {
+                nums1[mergedIndex] = nums1[nums1Index];
+                nums1Index--;
+            } else {
+                nums1[mergedIndex] = nums2[nums2Index];
+                nums2Index--;
+            }
+
+            mergedIndex--;
+        }
+    }
+}
