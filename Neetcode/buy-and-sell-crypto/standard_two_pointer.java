b1d2805149671b3a4e2e7931a5ea2ab2b487cb13
Swaroopvp24 <157394466+Swaroopvp24@users.noreply.github.com>
2026-08-20 22:22:04 +0530

    Add solution for buy-and-sell-crypto (standard_two_pointer.java)

---
+++ Neetcode/buy-and-sell-crypto/standard_two_pointer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int l = 0, r = 1;
+        while (r < prices.length) {
+            if (prices[r] > prices[l]) {
+                maxProfit = Math.max(maxProfit, prices[r] - prices[l]);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+        return maxProfit;
+    }
+}
