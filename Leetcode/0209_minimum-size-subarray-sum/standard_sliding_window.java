a6209aac77ce0b260e3cb5f63d343655a25b1386
Swaroopvp24 <157394466+Swaroopvp24@users.noreply.github.com>
2026-08-21 21:08:53 +0530

    Add solution for minimum-size-subarray-sum (standard_sliding_window.java)

---
+++ Leetcode/0209_minimum-size-subarray-sum/standard_sliding_window.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int l = 0, len = Integer.MAX_VALUE, sum = 0;
+        for (int r = 0; r < nums.length; r++) {
+            sum += nums[r];
+            while (sum >= target) {
+                len = Math.min(len, r - l + 1);
+                sum -= nums[l++];
+            }
+        }
+        return len == Integer.MAX_VALUE ? 0 : len;
+    }
+}
